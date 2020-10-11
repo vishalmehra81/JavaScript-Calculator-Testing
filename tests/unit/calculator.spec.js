c951067b1aa8a52472084e22740066d372ee6bb4
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-10-11 19:31:45 +0100

    unit test - add, subtract, multiply, divide

---
+++ tests/unit/calculator.spec.js
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  it('enterNum changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('5');
+    expect(wrapper.vm.runningTotal).to.equal(9)
+  })
+
+  it('should be able to add 1 to 4 resulting in 5', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 1
+    wrapper.vm.add('4');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  })
+
+  it('should be able to subtract 4 from 7 resulting in 3', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  it('should be able to multiply 3 with 5 resulting in 15', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  })
+
+  it('should be able to divide 21 by 7 resulting in 3', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+})
